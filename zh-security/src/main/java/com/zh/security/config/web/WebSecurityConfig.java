773498a61c733c28d05d2d5fadf6f8af6588c5b5
zhaomengxia <zhaomengxia@vortexinfo.cn>
2019-02-27 16:57:20 +0800

    使用内容安全策略防止XSS(跨站点脚本)和数据注入攻击

--- zh-security/src/main/java/com/zh/security/config/web/WebSecurityConfig.java
+++ zh-security/src/main/java/com/zh/security/config/web/WebSecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.web.authentication.AuthenticationFailureHand
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.session.InvalidSessionStrategy;
 import org.springframework.session.security.web.authentication.SpringSessionRememberMeServices;
 
@@ -132,14 +133,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .rememberMeServices(rememberMeServices)
                 .and()
                 .csrf().disable();// 关闭csrf防护
+        ////当请求通过HTTPS发生时，Spring Security会自动加入一个secure标识到XSRF-TOKENcookie 。Spring Security对于CSRF cookie不使用SameSite=strict 的标志，但它在使用Spring Session或WebFlux会话处理时会使用，
+        // 这对会话cookie有意义，因为它有助于识别用户，但是没有为CSRF cookie提供太多价值，因为CSRF令牌也需要在请求中
+//        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());//开启csrf防护
+
         //手机验证码登陆配置
         http.apply(smsAuthenticationSecurityConfig);
         //Spring Security * 默认情况下不添加CSP（内容安全策略）是一个增加的安全层，可帮助缓解CSS（跨站点脚本）和数据注入攻击
         //要启用该策略，需要配置应用程序以返回Content-Security-Policy标题。可以在 HTML页面中<meta http-equiv="Content-Security-Policy">使用标记
         //spring安全性默认提供了许多安全标头
-        //当请求通过HTTPS发生时，Spring Security会自动加入一个secure标识到XSRF-TOKENcookie 。Spring Security对于CSRF cookie不使用SameSite=strict 的标志，但它在使用Spring Session或WebFlux会话处理时会使用，
-        // 这对会话cookie有意义，因为它有助于识别用户，但是没有为CSRF cookie提供太多价值，因为CSRF令牌也需要在请求中
-        http.headers().contentSecurityPolicy("script-src 'self' https://trustedscripts.example.com; object-src https://trustedplugins.example.com; report-uri /csp-report-endpoint/");
+//        http.headers().contentSecurityPolicy("script-src 'self' https://trustedscripts.example.com; object-src https://trustedplugins.example.com; report-uri /csp-report-endpoint/");
 
     }
 
